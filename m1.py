e965a373fcb153d2f52ce7fb473ee8c0387e3a1f
Shiva Ram P <shivaramp0208@gmail.com>
2026-05-22 17:05:17 +0530

    Removed unnecessary files

--- m1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_csv(r"D:\COLLEGE\stock.txt")
-plt.plot(df['Date'], df['Open'], marker='o')
-plt.xlabel('Date')
-plt.ylabel('Open Price')
-plt.grid(True)
-plt.show()
-
